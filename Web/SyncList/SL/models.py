bd81f1abccc67209b4fda8412dd9e703ab643e64
Mitch Stephan <mitch.stephan@gmail.com>
2014-10-25 14:21:14 -0500

    Basic model implementation

--- Web/SyncList/SL/models.py
+++ Web/SyncList/SL/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    # id (pk)
+    email = models.CharField(max_length=255)
+    # should we have a password in alpha?
+    password = models.CharField(max_length=255)
+    
+
+
+class Task(models.Model):
+    # id (pk)
+    name = models.CharField(max_length=225)
+    # order of task in list
+    order = models.ImageField()
+    list = models.ForeignKey(List)
+    date_created = models.DateTimeField()
+    completed = models.BooleanField()
+    visible = models.BooleanField()
+
+
+
+class List(models.Model):
+    # id (pk)
+    # list of users shared
+    name = models.CharField(max_length=225)
+    owner = models.ForeignKey(User)
+    shared_users = models.ManyToManyField('User')
+    date_created = models.DateTimeField()
