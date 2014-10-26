f1da61b19ca4ab0bacc9e83e64e8300e92cacc68
Adam Hopkins <SirChickenHair@srchulo-aire.local>
2014-10-25 20:25:18 -0500

    Syncing SyncLists Activity for Ethan

--- Android/SyncLists/app/src/main/java/com/example/synclists/synclists/ListsActivity.java
+++ Android/SyncLists/app/src/main/java/com/example/synclists/synclists/ListsActivity.java
@@ -1,12 +1,19 @@
 package com.example.synclists.synclists;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.SimpleExpandableListAdapter;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,6 +28,7 @@ public class ListsActivity extends Activity{
     private ExpandableListView mExpandableListView;
     private List<String> mListDataHeader;
     private HashMap<String, List<String>> mListDataChild;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +36,10 @@ public class ListsActivity extends Activity{
         setContentView(R.layout.activity_sync_lists_lists);
 
         mExpandableListView = (ExpandableListView) findViewById(R.id.lists);
+        mButton = (Button) findViewById(R.id.addListButton);
         mListDataHeader = new ArrayList<String>();
         mListDataChild = new HashMap<String, List<String>>();
+
 
         populateLists();
 
@@ -55,5 +65,28 @@ public class ListsActivity extends Activity{
 
         mListDataChild.put(mListDataHeader.get(0), list1);
         mListDataChild.put(mListDataHeader.get(1), list2);
+    }
+
+    public void addList(View view) {
+        mButton.setEnabled(false);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.lists_layout);
+        EditText newList = new EditText(this);
+        newList.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        // set focus on newList
+        setEditTextFocus(newList, true);
+
+        layout.addView(newList);
+    }
+
+    private void setEditTextFocus(EditText et, boolean isFocused){
+        et.setCursorVisible(isFocused);
+        et.setFocusable(isFocused);
+        et.setFocusableInTouchMode(isFocused);
+
+        if (isFocused){
+            et.requestFocus();
+        }
     }
 }
