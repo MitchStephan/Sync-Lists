e267c0189e64d9a13d5240fd07825858c1e20f90
Adam Hopkins <violapiratejunky@gmail.com>
2014-10-26 21:32:22 -0500

    Now when user signs up or logins, user context is saved in shared preferences

--- Android/SyncLists/app/src/main/java/com/example/synclists/synclists/SyncListsCreateUserAsyncTask.java
+++ Android/SyncLists/app/src/main/java/com/example/synclists/synclists/SyncListsCreateUserAsyncTask.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 
 /**
  * Created by SirChickenHair on 10/26/14.
@@ -21,13 +22,21 @@ public class SyncListsCreateUserAsyncTask extends SyncListsRequestAsyncTask  {
             Toast.makeText(mActivity, "Account created!",
                     Toast.LENGTH_SHORT).show();
 
-            //Write out user_context
-            SharedPreferences.Editor editor = SyncListsMain.getPreferencesEditor();
-            editor.putInt(SyncListsApi.USER_CONTEXT, 1);
-            editor.commit();
+            try {
+                JSONObject jsonObject = new JSONObject(result.getBody());
 
-            Intent lists = new Intent(mActivity, ListsActivity.class);
-            mActivity.startActivity(lists);
+                //Write out user_context
+                SharedPreferences.Editor editor = SyncListsMain.getPreferencesEditor();
+                editor.putInt(SyncListsApi.USER_CONTEXT, jsonObject.getInt("pk"));
+                editor.commit();
+
+                Intent lists = new Intent(mActivity, ListsActivity.class);
+                mActivity.startActivity(lists);
+                return;
+            }
+            catch(Exception e) {
+                //if exception parsing json, then error loggin in
+            }
         }
         else {
             Toast.makeText(mActivity, "There was an error signing up",
--- Android/SyncLists/app/src/main/java/com/example/synclists/synclists/SyncListsLoginAsyncTask.java
+++ Android/SyncLists/app/src/main/java/com/example/synclists/synclists/SyncListsLoginAsyncTask.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 
 /**
  * Created by SirChickenHair on 10/26/14.
@@ -29,17 +30,22 @@ public class SyncListsLoginAsyncTask extends SyncListsRequestAsyncTask {
             Toast.makeText(mActivity, "You did it",
                     Toast.LENGTH_SHORT).show();
 
-            //Write out user_context
-            SharedPreferences.Editor editor = SyncListsMain.getPreferencesEditor();
-            editor.putInt(SyncListsApi.USER_CONTEXT, 1);
-            editor.commit();
+            try {
+                JSONObject jsonObject = new JSONObject(result.getBody());
 
-            Intent lists = new Intent(mActivity, ListsActivity.class);
-            mActivity.startActivity(lists);
-            return;
+                //Write out user_context
+                SharedPreferences.Editor editor = SyncListsMain.getPreferencesEditor();
+                editor.putInt(SyncListsApi.USER_CONTEXT, jsonObject.getInt("pk"));
+                editor.commit();
+
+                Intent lists = new Intent(mActivity, ListsActivity.class);
+                mActivity.startActivity(lists);
+                return;
+            }
+            catch(Exception e) {
+                //if exception parsing json, then error loggin in
+            }
         }
-
-
 
         Toast.makeText(mActivity, "Invalid login",
                 Toast.LENGTH_SHORT).show();
