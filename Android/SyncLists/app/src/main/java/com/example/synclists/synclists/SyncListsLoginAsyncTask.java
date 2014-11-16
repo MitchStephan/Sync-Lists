297c4a47720be3049649b604bb64e8a375279d1e
Adam Hopkins <violapiratejunky@gmail.com>
2014-11-15 20:20:52 -0600

    fixes #18

--- Android/SyncLists/app/src/main/java/com/example/synclists/synclists/SyncListsLoginAsyncTask.java
+++ Android/SyncLists/app/src/main/java/com/example/synclists/synclists/SyncListsLoginAsyncTask.java
@@ -1,6 +1,7 @@
 package com.example.synclists.synclists;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -13,9 +14,21 @@ import org.json.JSONObject;
  * Created by SirChickenHair on 10/26/14.
  */
 public class SyncListsLoginAsyncTask extends SyncListsRequestAsyncTask {
+    private ProgressDialog mProgressDialog;
 
     public SyncListsLoginAsyncTask(Activity activity) {
         super(activity);
+        mProgressDialog = new ProgressDialog(activity);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mProgressDialog.setMessage("Loading...");
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setCancelable(true);
+        mProgressDialog.show();
     }
 
     protected void onPostExecute(SyncListsResponse result) {
@@ -44,6 +57,9 @@ public class SyncListsLoginAsyncTask extends SyncListsRequestAsyncTask {
             }
         }
 
+        //only dismiss dialog on failure, because otherwise activity is exited and not saved in
+        //history
+        mProgressDialog.dismiss();
         Toast.makeText(mActivity, "Invalid login",
                 Toast.LENGTH_SHORT).show();
     }
